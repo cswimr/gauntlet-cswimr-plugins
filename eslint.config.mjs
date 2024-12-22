e53a289ea17940aacfdcd57e28743acc80b9b24b
cswimr <seaswimmerthefsh@gmail.com>
2024-12-22 16:46:54 -0500

    just eslint things

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginPrettierRecommended,
 ];
--- src/emojipicker.tsx
+++ src/emojipicker.tsx
@@ -1,4 +1,10 @@
-import { Action, ActionPanel, Content, Icons, Inline } from "@project-gauntlet/api/components";
+import {
+  Action,
+  ActionPanel,
+  Content,
+  Icons,
+  Inline,
+} from "@project-gauntlet/api/components";
 import React, { ReactNode } from "react";
 import { Clipboard, showHud } from "@project-gauntlet/api/helpers";
 import * as UnicodeEmoji from "unicode-emoji";
@@ -7,44 +13,44 @@ import * as UnicodeEmoji from "unicode-emoji";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const denoCore: DenoCore = Deno[Deno.internal].core;
 
-export default function EmojiPicker(props: { text: string }): ReactNode | undefined {
-    const text = props.text.trim();
+export default function EmojiPicker(props: {
+  text: string;
+}): ReactNode | undefined {
+  const text = props.text.trim();
 
-    if (text.length < 3) {
-        return undefined
-    };
+  if (text.length < 3) {
+    return undefined;
+  }
 
-    const emoji = UnicodeEmoji.getEmojis().find(emoji => emoji.keywords.includes(text));
-    if (!emoji) {
-        return undefined
-    };
+  const emoji = UnicodeEmoji.getEmojis().find((emoji) =>
+    emoji.keywords.includes(text),
+  );
+  if (!emoji) {
+    return undefined;
+  }
 
-    return (
-        <Inline
-            actions={
-                <ActionPanel>
-                    <Action
-                        label={`Copy ${emoji.emoji} to clipboard`}
-                        onAction={async () => {
-                            console.log(emoji.emoji)
-                            await Clipboard.writeText(emoji.emoji);
-                            showHud(`${emoji.emoji} copied to clipboard`);
-                        }}
-                    />
-                </ActionPanel>
-            }
-        >
-            <Inline.Left>
-                <Content.H3>
-                    {text}
-                </Content.H3>
-            </Inline.Left>
-            <Inline.Separator icon={Icons.ArrowRight}/>
-            <Inline.Right>
-                <Content.H3>
-                    {emoji.emoji}
-                </Content.H3>
-            </Inline.Right>
-        </Inline>
-    );
-};
+  return (
+    <Inline
+      actions={
+        <ActionPanel>
+          <Action
+            label={`Copy ${emoji.emoji} to clipboard`}
+            onAction={async () => {
+              console.log(emoji.emoji);
+              await Clipboard.writeText(emoji.emoji);
+              showHud(`${emoji.emoji} copied to clipboard`);
+            }}
+          />
+        </ActionPanel>
+      }
+    >
+      <Inline.Left>
+        <Content.H3>{text}</Content.H3>
+      </Inline.Left>
+      <Inline.Separator icon={Icons.ArrowRight} />
+      <Inline.Right>
+        <Content.Paragraph>{emoji.emoji}</Content.Paragraph>
+      </Inline.Right>
+    </Inline>
+  );
+}
